15d3cd3519c7d6787cc0211dd7a2a203d1c05b94
Samaksh Agarwal <agarwalsamaksh11@gmail.com>
2024-04-05 21:35:01 +0530

    Set up chakra ui and navbar component

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
+import { ChakraProvider } from '@chakra-ui/react';
+import Navbar from './components/Navbar/index.jsx';
+import Login from './components/Login/index.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <Navbar />
+      <App />
+      <Login />
+    </ChakraProvider>
   </React.StrictMode>
 );
